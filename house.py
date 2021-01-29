e529439066be3855a2fb0326393af307e87f81cf
ayannroys <73950076+ayannroys@users.noreply.github.com>
2021-01-29 22:14:03 +0530

    Add files via upload

---
+++ house.py
@@ -0,0 +1,21 @@
+red  = []
+blue = []
+yellow = []
+green = []
+ans = 1
+for i in range (0,10) :
+    name = (input("Enter your name : "))
+    house = (input("Enter your house : "))
+    if house == "red" :
+        red.append(name)
+    elif house == "blue":
+        blue.append(name)
+    elif house == "yellow":
+        yellow.append(yellow)
+    elif house == "green":
+        green.append(green)
+print("Red House = ",red, "\n Blue House = ",blue,"\n Yellow house = ",yellow,"\n Green House = ",green)
+
+
+
+
